76db83b93d189aa6e37ba6fb26e7a064ac45a004
Tomas <tomascorvalan14@gmail.com>
2024-06-27 16:25:59 -0400

    arreglos post almuerzo

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
 const card = document.querySelector(".cards-box");
 const mazos = [
   {
-    src: "../img/Purrely.jpeg",
+    src: "https://assetsio.gnwcdn.com/yu-gi-oh-tcg-tearlaments-kitkallos-art-crop.png?width=1200&height=1200&fit=crop&quality=100&format=png&enable=upscale&auto=webp",
     Nombre: "Tearlaments",
     Tipo: "Control/Aggro",
     Atributo: "Oscuridad",
@@ -10,13 +10,13 @@ const mazos = [
       "mazo centrado en botar cartas del mazo al cementerio para activar poderosos efectos que permitiran acceso al Mazo Extra invocando poderosos Monstruos de Fusión",
   },
   {
-    src: "../img/Purrely.jpeg",
+    src: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cf713fd6-bbfa-4049-9ccf-a366f09ebe02/dgg6aqo-e8a7ba10-1601-460f-8dd2-bb55d0022ecb.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2NmNzEzZmQ2LWJiZmEtNDA0OS05Y2NmLWEzNjZmMDllYmUwMlwvZGdnNmFxby1lOGE3YmExMC0xNjAxLTQ2MGYtOGRkMi1iYjU1ZDAwMjJlY2IuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.NflpTahe8MU7sPXimyae_Gj3xnxyL3aWWfe-lLBnOWk",
     Nombre: "Mementotlan",
     Tipo: "Control",
     Atributo: "Oscuridad",
     Valor: "100 USD",
     descr:
-      "mazo centrado en botar cartas del mazo al cementerio para activar poderosos efectos que permitiran acceso al Mazo Extra invocando poderosos Monstruos de Fusión",
+      "mazo centrado en generar recursos para invocar a Mementoal Tecuhtlica - Combined Creation ",
   },
   {
     src: "https://cdn.ygorganization.com/2023/07/purrely.jpg",
@@ -25,7 +25,7 @@ const mazos = [
     Atributo: "Luz",
     Valor: "150 USD",
     descr:
-      "mazo centrado en botar cartas del mazo al cementerio para activar poderosos efectos que permitiran acceso al Mazo Extra invocando poderosos Monstruos de Fusión",
+      "¿Quién necesita grandes monstruos cuando puedes tener gatitos",
   },
 ];
 let cardContent = "";
@@ -45,31 +45,31 @@ card.innerHTML = cardContent;
 const card2 = document.querySelector(".cards-box2");
 const arriendos = [
   {
-    src: "../img/Purrely.jpeg",
-    Nombre: "Tearlaments",
+    src: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/13714538-e319-4cce-9062-6941ce18525d/df4q9nw-1c9366cc-9ad5-4b04-b757-d8714061c6ed.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzEzNzE0NTM4LWUzMTktNGNjZS05MDYyLTY5NDFjZTE4NTI1ZFwvZGY0cTludy0xYzkzNjZjYy05YWQ1LTRiMDQtYjc1Ny1kODcxNDA2MWM2ZWQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.HMhnlOWm1P4h1kaqAJiOaHmJp6khJ7wMBNa46Dwt2D0",
+    Nombre: "Magistus",
     Tipo: "Control/Aggro",
     Atributo: "Oscuridad",
     Valor: "200 USD",
     descr:
-      "mazo centrado en botar cartas del mazo al cementerio para activar poderosos efectos que permitiran acceso al Mazo Extra invocando poderosos Monstruos de Fusión",
+      "Rituals. Fusions. Synchros. Xyz. It’s so hard to choose a direction for your Deck, but Magikeys open the door to all these possibilities!",
   },
   {
-    src: "../img/Purrely.jpeg",
-    Nombre: "Mementotlan",
+    src: "https://preview.redd.it/wm67a5gsr8k81.jpg?auto=webp&s=03152669ab6ceb697127a274b44a14cf14d7ec66",
+    Nombre: "S-Force",
     Tipo: "Control",
     Atributo: "Oscuridad",
     Valor: "100 USD",
     descr:
-      "mazo centrado en botar cartas del mazo al cementerio para activar poderosos efectos que permitiran acceso al Mazo Extra invocando poderosos Monstruos de Fusión",
+      "The S-Force monsters utilize the field’s columns to control the game. Any opposing monster that shares a column with an S-Force will have some kind of negative effect applied.",
   },
   {
-    src: "https://cdn.ygorganization.com/2023/07/purrely.jpg",
-    Nombre: "Purrely",
+    src: "https://ih1.redbubble.net/image.1646278449.6602/st,small,507x507-pad,600x600,f8f8f8.jpg",
+    Nombre: "Toon",
     Tipo: "Control",
     Atributo: "Luz",
     Valor: "150 USD",
     descr:
-      "mazo centrado en botar cartas del mazo al cementerio para activar poderosos efectos que permitiran acceso al Mazo Extra invocando poderosos Monstruos de Fusión",
+      "¿Quieres sentirte como Maximilian Pegasus? Con este mazo Toon podras hacer todas las jugadas que quieras",
   },
 ];
 let cardContent2 = "";
